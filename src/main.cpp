53672ee8fbf3ab141062aa0682cab9b4c505a2a2
Nguyen Trong Nghia <projects.futuresky@gmail.com>
2019-07-15 20:42:54 +0700

    Run manually code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
-#include <GsmMqtt.h>
 #include <SoftwareSerial.h>
+#include <stdio.h>
 
 #define DEBUG true
 #define gpsPin 5
@@ -14,38 +14,26 @@
 
 SoftwareSerial mySerial(RX, TX);
 /*------------- USER INPUT --------------------------------*/
-char *deviceName;
-char *mqttBroker; // mqtt broker ip
-char *mqttPort;   //mqtt broker port
-char *mqttUser;
-char *mqttPass;
-char *mqttTopic;
-String sosNum = "";
+String deviceIMEI = "";
+char *serverAddress; // mqtt broker ip
+char *serverPort;    //mqtt broker port
+char *gpsParamPrefix;
+char *imeiParamPrefix;
 /*---------------------------------------------------------*/
 String GpsData = "";
 //char Message[300];
 int prefixIndex = -1, surfixIndex = -1;
 byte inCommingByte;
 char atCommand[50];
-byte mqttMessage[127];
-int mqttMessageLength = 0;
 
 //bolean flags
 boolean gprsReady = false;
-boolean mqttSent = false;
-boolean isGetted = false;
 
-void hangup(void);
-void call(void);
-void sos(void);
-String buildJson(void);
-void sendMQTTMessage(char *, char *, char *, char *, char *);
 boolean isGPRSReady(void);
-void sendGprsData(void);
 String sendData(String, const int, boolean);
 void checkGpsStatus(void);
 void getIMEI(void);
-void setMqttSetup(void);
+void getSetUp(void);
 void pwrGps(void);
 void getGpsData(void);
 
@@ -67,7 +55,7 @@ void setup()
   digitalWrite(TN_SEND_DATA_PIN, LOW);
   digitalWrite(TN_SERVER_CONNECTED_PIN, LOW);
   digitalWrite(TN_SIM_ON_PIN, LOW);
-  
+
   delay(500);
 
   Serial.begin(9600);   // Đặt serial baudrate của UNO là 9600
@@ -78,33 +66,33 @@ void setup()
     delay(1000);
   }
 
-  digitalWrite(TN_SIM_ON_PIN, HIGH);
+  sendData("ATE0", 1000, DEBUG);
 
-  if (!isGetted)
-  {
-    getIMEI();
-    setMqttSetup();
-    isGetted = true;
-    setup();
-  }
-  else
-  {
-    pwrGps();
-    Serial.println("Finish set up!");
-  }
+  digitalWrite(TN_SIM_ON_PIN, HIGH);
+  getIMEI();
+  getSetUp();
+  pwrGps();
+  Serial.println("Finish set up!");
 }
 
 void loop()
 {
   getGpsData();
-  sendGprsData();
+  sendData("AT+HTTPINIT", 1200, DEBUG);
+  String build = String("AT+HTTPPARA=\"URL\",\"http://") + String(serverAddress) + String(":") + String(serverPort) + String("/?") + String(gpsParamPrefix) + String("=") + String(GpsData) + String("&") + String(imeiParamPrefix) + String("="); //init the HTTP request
+  build = String(build + deviceIMEI + String("\""));
+  // String(deviceIMEI) +
+  sendData(build, 4000, DEBUG);             // setting the httppara,
+  sendData("AT+HTTPACTION=0", 3000, DEBUG); //submit the GET request
+  String x = sendData("AT+HTTPREAD", 1000, DEBUG);
+  Serial.println(x);
+  sendData("AT+HTTPTERM", 1000, true); // terminate HTTP service                                                                     // read the data from the website you access
+  //  sendGprsData();
 }
 
 void getIMEI()
 {
   String x = sendData("AT+GSN", 1000, true);
-  x.replace("\r\n", "");
-  x = x.substring(x.indexOf("AT+GSN") + 6, x.indexOf("OK"));
   if (x.length() < 15)
   {
     getIMEI();
@@ -112,13 +100,21 @@ void getIMEI()
   }
   else
   {
-    x = "IMEI" + x;
-    deviceName = new char[x.length() + 1];
-    x.toCharArray(deviceName, x.length() + 1);
+    int firstIndex = -1;
+    for (int i = 0; i < x.length(); i++)
+    {
+      if (x.charAt(i) >= '0' && x.charAt(i) <= '9')
+      {
+        firstIndex = i;
+        break;
+      }
+    }
+    deviceIMEI = x.substring(firstIndex, firstIndex + 15);
+    Serial.println("[" + deviceIMEI + "]");
     delay(100);
   }
 }
-void setMqttSetup()
+void getSetUp()
 {
   sendData("AT+CGATT=0", 1000, true);
   delay(1000);
@@ -145,37 +141,27 @@ void setMqttSetup()
   {
     if (i == 0)
     {
-      mqttBroker = new char[strlen(pch) + 1];
-      strncpy(mqttBroker, pch, strlen(pch));
-      mqttBroker[strlen(pch)] = 0;
+      serverAddress = new char[strlen(pch) + 1];
+      strncpy(serverAddress, pch, strlen(pch));
+      serverAddress[strlen(pch)] = 0;
     }
     else if (i == 1)
     {
-      mqttPort = new char[strlen(pch) + 1];
-      strncpy(mqttPort, pch, strlen(pch));
-      mqttPort[strlen(pch)] = 0;
+      serverPort = new char[strlen(pch) + 1];
+      strncpy(serverPort, pch, strlen(pch));
+      serverPort[strlen(pch)] = 0;
     }
     else if (i == 2)
     {
-      mqttUser = new char[strlen(pch) + 1];
-      strncpy(mqttUser, pch, strlen(pch));
-      mqttUser[strlen(pch)] = 0;
+      gpsParamPrefix = new char[strlen(pch) + 1];
+      strncpy(gpsParamPrefix, pch, strlen(pch));
+      gpsParamPrefix[strlen(pch)] = 0;
     }
     else if (i == 3)
     {
-      mqttPass = new char[strlen(pch) + 1];
-      strncpy(mqttPass, pch, strlen(pch));
-      mqttPass[strlen(pch)] = 0;
-    }
-    else if (i == 4)
-    {
-      mqttTopic = new char[strlen(pch) + 1];
-      strncpy(mqttTopic, pch, strlen(pch));
-      mqttTopic[strlen(pch)] = 0;
-    }
-    else if (i == 5)
-    {
-      sosNum = "ATD" + String(pch) + ";";
+      imeiParamPrefix = new char[strlen(pch) + 1];
+      strncpy(imeiParamPrefix, pch, strlen(pch));
+      imeiParamPrefix[strlen(pch)] = 0;
     }
     pch = strtok(NULL, " ");
     i++;
@@ -183,16 +169,14 @@ void setMqttSetup()
   if (DEBUG)
   {
     Serial.println("INFOMATION");
-    Serial.println("IMEI: " + String(deviceName));
-    Serial.println("Broker: " + String(mqttBroker));
-    Serial.println("Port: " + String(mqttPort));
-    Serial.println("User: " + String(mqttUser));
-    Serial.println("Pass: " + String(mqttPass));
-    Serial.println("Topic: " + String(mqttTopic));
-    Serial.println("SOS Num: " + String(sosNum));
+    Serial.println("IMEI: " + String(deviceIMEI));
+    Serial.println("Broker: " + String(serverAddress));
+    Serial.println("Port: " + String(serverPort));
+    Serial.println("User: " + String(gpsParamPrefix));
+    Serial.println("Pass: " + String(imeiParamPrefix));
   }
   sendData("AT+HTTPTERM", 1000, true); // terminate HTTP service
-  sendData("AT+SAPBR=0,1", 1000, true);
+  // sendData("AT+SAPBR=0,1", 1000, true);
 }
 void checkGpsStatus()
 {
@@ -252,35 +236,6 @@ String sendData(String command, const int timeout, boolean debug)
   return response;
 }
 
-void sendGprsData()
-{
-  gprsReady = isGPRSReady();
-  if (gprsReady == false)
-  {
-    Serial.println("GPRS Not Ready");
-  }
-  if (gprsReady == true)
-  {
-    prefixIndex = -1;
-    surfixIndex = -1;
-    // Cut prefix
-    prefixIndex = GpsData.indexOf("+CGNSINF:");
-    surfixIndex = GpsData.lastIndexOf("OK");
-    surfixIndex = (surfixIndex >= 0 && surfixIndex < prefixIndex ? surfixIndex + 2 : GpsData.length());
-    if (prefixIndex >= 0)
-    {
-      GpsData = GpsData.substring(prefixIndex, surfixIndex);
-      GpsData.replace("\r\n", ""); // delete all new line
-      Serial.println(GpsData);
-    }
-    // Concert data
-    char gpsData[GpsData.length() + 1];
-    GpsData.toCharArray(gpsData, GpsData.length() + 1);
-    //clientID, IP, Port, Topic, Message
-    sendMQTTMessage(deviceName, mqttBroker, mqttPort, mqttTopic, gpsData);
-  }
-}
-
 boolean isGPRSReady()
 {
   mySerial.println("AT");
@@ -293,95 +248,25 @@ boolean isGPRSReady()
     return false;
 }
 
-void blinkDataLed(int seconds)
-{
-  for (int i = 0; i < seconds * 10; i++)
-  {
-    digitalWrite(TN_SEND_DATA_PIN, LOW);
-    delay(50);
-    digitalWrite(TN_SEND_DATA_PIN, HIGH);
-    delay(50);
-  }
-}
-void sendMQTTMessage(char *clientId, char *brokerUrl, char *brokerPort, char *topic, char *message)
-{
-  digitalWrite(TN_SEND_DATA_PIN, HIGH);
-  mySerial.println("AT"); // Sends AT command to wake up cell phone
-  Serial.println("send AT to wake up mySerial");
-  delay(1000); // Wait a second
-  // digitalWrite(13, HIGH);
-  mySerial.println("AT+CSTT=\"www\",\"\",\"\""); // Puts phone into mySerial mode
-  Serial.println("AT+CSTT=\"www\",\"\",\"\"");
-  delay(2000); // Wait a second
-  mySerial.println("AT+CIICR");
-  Serial.println("AT+CIICR");
-  delay(2000);
-  mySerial.println("AT+CIFSR");
-  Serial.println("AT+CIFSR");
-  delay(1000);
-  strcpy(atCommand, "AT+CIPSTART=\"TCP\",\"");
-  strcat(atCommand, brokerUrl);
-  strcat(atCommand, "\",\"");
-  strcat(atCommand, brokerPort);
-  strcat(atCommand, "\"");
-  if (sendData(atCommand, 2000, DEBUG).indexOf("CONNECT") < 0)
-  {
-    digitalWrite(TN_SERVER_CONNECTED_PIN, LOW);
-    delay(1000);
-    return;
-  }
-  else
-    digitalWrite(TN_SERVER_CONNECTED_PIN, HIGH);
-
-  // mySerial.println(atCommand);
-  // Serial.println(atCommand);
-  // delay(1000);
-
-  mySerial.println("AT+CIPSEND");
-  Serial.println("AT+CIPSEND");
-  delay(1000);
-
-  mqttMessageLength = 14 + strlen(clientId);
-  Serial.println(mqttMessageLength);
-  mqtt_connect_message(mqttMessage, clientId);
-  for (int j = 0; j < mqttMessageLength; j++)
-    mySerial.write(mqttMessage[j]); // Message contents
-  mySerial.write(byte(26));         // (signals end of message)
-  Serial.println("Sending message");
-
-  blinkDataLed(1);
-  digitalWrite(TN_SEND_DATA_PIN, LOW);
-  mySerial.println("AT+CIPSEND");
-  Serial.println("AT+CIPSEND");
-  delay(1000);
-  mqttMessageLength = 4 + strlen(topic) + strlen(message);
-  mqtt_publish_message(mqttMessage, topic, message);
-  for (int k = 0; k < mqttMessageLength; k++)
-    mySerial.write(mqttMessage[k]);
-  mySerial.write(byte(26)); // (signals end of message)
-  Serial.println("");
-  Serial.println("-------------Sent-------------"); // Message contents
-  blinkDataLed(2);
-}
-
-void call(void)
-{
-  sendData("AT+CSQ", 1000, DEBUG); // Kiểm tra chất lượng tín hiệu
-  Serial.println("Making a call!");
-  mySerial.println(sosNum); // xxxxxxxxx là số mà muốn sim thực hiện gọi đến (Ở đây là số của Nghĩa :D)
-  if (mySerial.available())
-    Serial.print((unsigned char)mySerial.read());
-}
-
-void hangup(void)
-{
-  mySerial.println("ATH"); // Kết thúc cuộc gọi
-  if (mySerial.available())
-    Serial.print((unsigned char)mySerial.read());
-}
 void getGpsData()
 {
   checkGpsStatus();
-  //+CGNSINF: 1,1,20161122182451.000,13.019292,77.686463,919.200,0.15,10.5,1,,0.9,2.0,1.8,,12,9,,,47,,
   GpsData = sendData("AT+CGNSINF", 1000, DEBUG);
+  //+CGNSINF: 1,1,20161122182451.000,13.019292,77.686463,919.200,0.15,10.5,1,,0.9,2.0,1.8,,12,9,,,47,,
+  prefixIndex = -1;
+  surfixIndex = -1;
+  // Cut prefix
+  prefixIndex = GpsData.indexOf("+CGNSINF:");
+  surfixIndex = GpsData.lastIndexOf("OK");
+  surfixIndex = (surfixIndex >= 0 && surfixIndex < prefixIndex ? surfixIndex + 2 : GpsData.length());
+  if (prefixIndex >= 0)
+  {
+    GpsData = GpsData.substring(prefixIndex, surfixIndex);
+    GpsData.replace("\r\n", ""); // delete all new line
+    GpsData.replace(" ", "");
+  }
+  else
+  {
+    GpsData = "NULL";
+  }
 }
